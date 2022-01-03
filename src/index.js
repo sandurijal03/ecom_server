5004799854d2ffc618952d9c19f996487f4f93d9
Sandip Rijal <sandurijal03@hotmail.com>
2022-01-03 21:05:43 +0545

    database packages added and helper function to connect to database added with function imported in main file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const connectDB = require('./db/connectDB');
 const router = require('./routes');
 
 const app = express();
 
 app.use(router);
+
+connectDB();
 
 const port = process.env.PORT || 8848;
 
