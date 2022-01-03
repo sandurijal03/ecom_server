6cd810b396597892a45a2c7cc80ff747370bda5f
Sandip Rijal <sandurijal03@hotmail.com>
2022-01-03 22:09:36 +0545

    main path changed

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ require('dotenv').config({});
 
 const express = require('express');
 const connectDB = require('./db/connectDB');
-const router = require('./routes');
+const mainRouter = require('./routes');
 
 const app = express();
 
 const mount = () => {
   try {
     connectDB();
-    app.use(router);
+    app.use('/api', mainRouter);
     const port = process.env.PORT;
     app.listen(port, () => console.log(`[server]: listening on ${port}`));
   } catch (err) {
